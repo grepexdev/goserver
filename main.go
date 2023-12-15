6039f3bf48cefe0fdf17719e67c72773e5c3ee40
grepexdev <grepexdev@gmail.com>
2023-12-14 21:06:36 -0600

    Add .env file and set PORT environment variable

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"time"
 )
 
@@ -12,8 +13,8 @@ func main() {
 	corsMux := middlewareCors(m)
 
 	m.HandleFunc("/", handlePage)
-
-	const addr = ":8080"
+	port := os.Getenv("PORT")
+	addr := ":" + port
 	srv := http.Server{
 		Handler:      corsMux,
 		Addr:         addr,
